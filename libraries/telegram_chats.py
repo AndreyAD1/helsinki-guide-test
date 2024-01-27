660f387830f1a7df1b6d34e8c1f31feb2889261d
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2024-01-27 18:18:43 +0200

    verify the setting buttons

--- libraries/telegram_chats.py
+++ libraries/telegram_chats.py
@@ -1,3 +1,18 @@
-def verify_settings_response(response_message):
-    print(response_message)
-    print(type(response_message))
+"""A library for processing Telegram chat messages."""
+from robot.api.deco import keyword
+from selenium.webdriver.remote.webelement import WebElement, By
+
+ROBOT_LIBRARY_VERSION = '0.1'
+ROBOT_AUTO_KEYWORDS = False
+
+
+@keyword
+def verify_settings_response(response_message: WebElement):
+    buttons = response_message.find_elements(
+        by=By.CSS_SELECTOR,
+        value=".InlineButtons .row .Button",
+    )
+    expected_button_names = ["Finnish", "English", "Russian"]
+    for expected_name, button in zip(expected_button_names, buttons):
+        err_message = f"Unexpected button text: {expected_name}. Expect: {button.text}."
+        assert expected_name == button.text, err_message
